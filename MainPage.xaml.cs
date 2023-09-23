bc1cd261319310cb312e65f4804fcd8600820374
Anderson Pinho <apinho@jbssa.com>
2023-09-23 15:00:07 -0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Maui.PhoneNumber
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber = string.Empty;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate_Clicked(object sender, EventArgs e)
+        {
+            string inputtedNumber = PhoneNumberText.Text;
+            translatedNumber = PhoneWordTranslator.ToNumber(inputtedNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                //CallButton.Text = $"Call to {translatedNumber}";
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+            }   CallButton.Text = "Call";
+        }
+
+        private async void OnCall_Clicked(object sender, EventArgs e)
+        {
+            if(await this.DisplayAlert(
+                "Dial number",
+                $"Would you like to call {translatedNumber} ?",
+                "Yes", "No"
+                ))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentException) 
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "Ok");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed", "Ok");
+                }
+            }
+        }
+    }
+}
---
+++ PhoneWordTranslator.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Maui.PhoneNumber;
+
+public static class PhoneWordTranslator
+{
+    static readonly string[] digits = { "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
+    public static string ToNumber(string raw)
+    {
+        if (string.IsNullOrWhiteSpace(raw))
+            return null;
+
+        raw = raw.ToUpperInvariant();
+
+        var newNumber = new StringBuilder();
+        foreach (var c in raw)
+        {
+            if (" -123456789".ContainsChar(c))
+                newNumber.Append(c);
+            else
+            {
+                var result = TranslateToNumber(c);
+                if (result != null)
+                    newNumber.Append(result);
+                else
+                    return null;
+            }
+        }
+        return newNumber.ToString();
+    }
+
+    static bool ContainsChar(this string keyString, char c) => keyString.IndexOf(c) >= 0;
+
+    static int? TranslateToNumber(char c)
+    {
+        for (var i = 0; i < digits.Length; i++)
+            if (digits[i].Contains(c))
+                return 2 + i;
+
+        return null;
+    }
+}
